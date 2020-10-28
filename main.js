25820268235e3b35f2a4cbb1ff36a590008e978e
creator200 <73328910+creator200@users.noreply.github.com>
2020-10-28 16:40:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+<script type="text/javscript">
+    </script>
+function cSearch() {
+    var get = document.getElementById("getValue").Value
+if (get = "hello"); {
+      window.alert("Hey! how are you");
+    }
+else if (get == "how are you");{
+    window.alert("I'm fine thanks for asking");
+}
